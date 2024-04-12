fb948b002ecb23c9de59ef91e9541d9b5c85d6de
ai1002hash <glenganderton@proton.me>
2024-04-12 11:47:31 +1000

    Homework 1 complete. Ready to merge into main branch.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,8 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            string firstName;
+            string lastName;
+            int age;
+            int weight;
+            double height;
+            int loopTwice = 0;
+
+            while (loopTwice < 2)
+            {
+                Console.Write("Please enter your name:");
+                firstName = Console.ReadLine();
+
+                Console.Write("Please enter your last name:");
+                lastName = Console.ReadLine();
+
+                Console.Write("Please enter your age:");
+                age = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter your weight in kg:");
+                weight = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter your height:");
+                height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight}kg and his height is {height}cm");
+                loopTwice++;
+            }
+
         }
     }
 }
